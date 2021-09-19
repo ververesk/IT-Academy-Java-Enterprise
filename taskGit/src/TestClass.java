b06917039bdb26a152f5c02aa628cfc2031a57dd
Veronika Hryharovich <weronnika@list.ru>
2021-09-19 19:46:31 +0300

    created new class cube

--- taskGit/src/TestClass.java
+++ taskGit/src/TestClass.java
@@ -1,8 +1,13 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println(square());
+        System.out.println(cube());
     }
     private static int square() {
         return 2*2;
+    }
+
+    private static int cube() {
+        return 2*2*2;
     }
 }
