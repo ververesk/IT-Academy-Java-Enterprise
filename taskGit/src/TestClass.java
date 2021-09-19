7aef75a162c0dff90e2d5a2c1f0a4d8551d561c1
Veronika Hryharovich <weronnika@list.ru>
2021-09-19 19:40:31 +0300

    created TestClass and changed .gitignore in branch main

---
+++ taskGit/src/TestClass.java
@@ -0,0 +1,8 @@
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println(square());
+    }
+    private static int square() {
+        return 2*2;
+    }
+}
