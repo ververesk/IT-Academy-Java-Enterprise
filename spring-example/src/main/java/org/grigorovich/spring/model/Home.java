25d50a2029d02d56de735ec8a586ec4ed6148893
Veronika Hryharovich <weronnika@list.ru>
2021-12-04 18:00:12 +0300

    Custom bean Qualifiers

--- spring-example/src/main/java/org/grigorovich/spring/model/Home.java
+++ spring-example/src/main/java/org/grigorovich/spring/model/Home.java
@@ -3,6 +3,7 @@ package org.grigorovich.spring.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.grigorovich.spring.qualifiers.IgorQualifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -13,7 +14,8 @@ public class Home {
     private int id;
     private int square;
     @Autowired
-    @Qualifier("123")
+ //   @Qualifier("123")
+    @IgorQualifier
     private Owner owner;
  //   private Owner igor;
 
