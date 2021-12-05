7ce2e8d802f0f6b6adfe4fc15ba63ce803ea35e6
Veronika Hryharovich <weronnika@list.ru>
2021-12-05 21:14:39 +0300

    PostConstruct/PreDestroy  methods Autowiring by constructor Collection external injection (Map)

--- Java-based/src/main/java/org/grigorovich/java_based/model/Home.java
+++ Java-based/src/main/java/org/grigorovich/java_based/model/Home.java
@@ -1,14 +1,16 @@
 package org.grigorovich.java_based.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.grigorovich.java_based.qualifiers.VeronikaQualifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Map;
 
 
 @Data
@@ -21,19 +23,48 @@ public class Home {
     private int id;
     @Value("${veronikaHome.square}")
     private int square;
-  //  @Autowired
-  //  @VeronikaQualifier ну или такой вариант
- //   @Qualifier("igor") //указала какой именно бин нам нужен
+    //  @Autowired
+    //  @VeronikaQualifier ну или такой вариант
+    //   @Qualifier("igor") //указала какой именно бин нам нужен
     private Owner owner;
+
+    @Autowired
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Map<String, Owner> ownerMap;
+
+    @Value("${veronikaHome.owner}")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private String ownerName;
+
 
     public Home(int id, int square) {
         this.id = id;
         this.square = square;
     }
+//Autowiring by setters
+//    @Autowired
+//    @Qualifier("igor")
+//    public void setOwner(Owner owner) {
+//        this.owner = owner;
+//    }
 
-    @Autowired
-    @Qualifier("igor")
-    public void setOwner(Owner owner) {
-        this.owner = owner;
+    //Autowiring by constructor
+//    @Autowired
+//    public Home(@Qualifier("veronika") Owner owner) {
+//        this.owner = owner;
+//    }
+
+    @PostConstruct
+    public void start() {
+        owner = ownerMap.get(ownerName);
+        System.out.println(this.getClass().getSimpleName() + ": init method");
+    }
+
+
+    @PreDestroy
+    public void finish() {
+        System.out.println(this.getClass().getSimpleName() + ": destroy method");
     }
 }
